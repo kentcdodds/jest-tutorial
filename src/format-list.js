8f1eb3d0df013b0435cd0e76461fb3256b2714f2
Kent C. Dodds <kent@doddsfamily.us>
2016-09-09 10:02:47 -0600

    indent list

--- src/format-list.js
+++ src/format-list.js
@@ -3,7 +3,7 @@ export default formatList
 function formatList(listName, items, key) {
   return `These are the items in the ${listName}:${
     items.reduce((itemsList, item) => {
-      return `${itemsList}\n- ${key ? item[key] : item}`
+      return `${itemsList}\n  - ${key ? item[key] : item}`
     }, '')
   }`
 }
